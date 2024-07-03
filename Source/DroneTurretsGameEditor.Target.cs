ddb6ab5d9ec95e815455bc6d434e55312f44587a
Kyrylo Volnykov <kvolnykov@gmail.com>
2024-07-03 11:42:39 +0300

    Upgrade project to v5.4

--- Source/DroneTurretsGameEditor.Target.cs
+++ Source/DroneTurretsGameEditor.Target.cs
@@ -8,8 +8,8 @@ public class DroneTurretsGameEditorTarget : TargetRules
 	public DroneTurretsGameEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("DroneTurretsGame");
 	}
 }
